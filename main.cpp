c0974519dc53cbd055b291533b13e682aa563afe
Andras Banki Horvath <bhandras@gmail.com>
2017-08-17 14:55:15 +0200

    Glorot initialization

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include "graph.h"
 #include "kernel.h"
 #include "ndarray.h"
@@ -8,10 +9,12 @@ void test();
 std::vector<VariableRef> variables;
 
 OpRef Linear(OpRef x, size_t inp_size, size_t out_size) {
+  float stddev = std::sqrt(6.0f / (inp_size + out_size));
+  
   auto W = Variable::create(x->graph(), Shape({inp_size, out_size}));
   variables.push_back(W);
   W->set_value(NDArray({inp_size, out_size},
-        random_normal_vec<float>(inp_size * out_size, 0.0f, 0.1f)));
+        random_normal_vec<float>(inp_size * out_size, 0.0f, stddev)));
 
   auto b = Variable::create(x->graph(), {out_size});
   variables.push_back(b);
@@ -25,7 +28,7 @@ void sgd(GraphRef g, float learning_rate) {
   for (auto& var : variables) {
     auto v = var->get_value();
     const auto& batch_grad = g->gradient(var);
-    //std::cout << "grad: " << batch_grad.reduce_sum() << std::endl;
+    //std::cout << "sum(grad): " << batch_grad.reduce_sum() << std::endl;
     v.sub_(batch_grad.muls(learning_rate));
     var->set_value(v);
   }
@@ -94,14 +97,16 @@ int main() {
   auto X = Variable::create(g, {28 * 28});
   auto y = Variable::create(g, {10});
 
-  auto l1 = Linear(X, 28*28, 100)->relu();
-  auto l2 = Linear(l1, 100, 10);
-  auto loss = l2->softmax_ce(y);
-  auto pred = l2->softmax();
+  auto l1 = Linear(X, 28*28, 512)->relu();
+  auto l2 = Linear(l1, 512, 512)->relu();
+  auto l3 = Linear(l1, 512, 10);
+  auto loss = l3->softmax_ce(y);
+  auto pred = l3->softmax();
   
   size_t batch_size = 100;
   size_t classes = 10;
   int steps = 1000;
+  float epoch = 0;
 
   for (int i = 0; i < steps; ++i) {
     auto batch = mnist.get_train_batch(batch_size);
@@ -111,11 +116,13 @@ int main() {
     X->set_value(NDArray({batch_size, 28*28}, batch_X));
     y->set_value(one_hot(batch_size, classes, batch_y));
     g->forward();
-   
-    std::cout << "loss: " << loss->get_value() << std::endl;
+
+    epoch += float(batch_size) / float(mnist.train_size);
+    std::cout << std::fixed << std::setw(6) << std::setprecision(6)
+      << "epoch: " << epoch << "\tloss: " << loss->get_value() <<std::endl;
     print_stat(batch_y, pred->get_value());
     g->backward(loss);
-    sgd(g, 0.025);
+    sgd(g, 0.01);
   }
   //std::cout << pred->get_value() << std::endl;
   return 0;
